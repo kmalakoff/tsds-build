4147f33a79d81872aedc2d90dbc47e6bdbb4e0ff
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-04 07:03:32 -0800

    Fix rollup

--- src/lib/rollingOptions.ts
+++ src/lib/rollingOptions.ts
@@ -3,14 +3,15 @@ import camelcase from 'lodash.camelcase';
 import path from 'path';
 import loadConfigSync from 'read-tsconfig-sync';
 
-import type { Package } from 'tsds-lib';
+import { loadConfig, type Package } from 'tsds-lib';
 
 export const pkg = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8')) as Package;
-export const source = (pkg.tsds ? pkg.tsds.source || 'src/index.ts' : 'src/index.ts') as string;
+const config = loadConfig();
+if (!config?.source) throw new Error('Missing "source" in package.json or .tsdsrc.json. Add "source": "src/index.ts" (or .tsx) to your config.');
+export const source = config.source;
 export const input = path.join.apply(null, [process.cwd(), ...source.split('/')]) as string;
 export const name = camelcase(pkg.name) as string;
-const globals_ = pkg.tsds ? pkg.tsds.globals || {} : {};
-export const globals = globals_ as Record<string, string>;
+export const globals = (config?.globals || {}) as Record<string, string>;
 
 const DEPS = ['dependencies', 'optionalDependencies', 'peerDependencies'];
 DEPS.forEach((x) => {
