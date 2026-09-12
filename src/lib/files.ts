30a84ca49a25084ef3f74842c331d2e217a2967b
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 20:54:15 -0700

    Initialize repository with CI, LICENSE, and fs-copy-compat

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import { copyFile } from 'fs-copy-compat';
 import Iterator, { type Entry } from 'fs-iterator';
 import mkdirp from 'mkdirp-classic';
 import path from 'path';
@@ -52,7 +53,7 @@ export default function files(_args: string[], type: TargetType, options: Comman
           const relative = path.relative(dest, path.dirname(entry.fullPath));
           const sourcePath = path.join(dest, relative, `${entry.basename.slice(0, -ext.length)}.d.ts`);
           const destPath = path.join(dest, relative, `${entry.basename.slice(0, -ext.length)}.d.${ext === '.js' ? 'cts' : 'mts'}`);
-          fs.copyFile(sourcePath, destPath, (err) => (err && err.code !== 'ENOENT' ? cb(err) : cb()));
+          copyFile(sourcePath, destPath, (err) => (err && err.code !== 'ENOENT' ? cb(err) : cb()));
         },
         { callbacks: true, concurrency: Infinity },
         callback
